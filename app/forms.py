b2465307b2eb82ecb73999de9ef4c51f4be3a30d
sandeep-1829 <godugunurusandeep@gmail.com>
2024-01-22 11:24:24 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -6,10 +6,11 @@ class UserForm(forms.ModelForm):
     class Meta:
         model=User
         fields=['username','email','password']
-        # help_texts={'username':''}
+        help_texts={'username':''}
         widgets={'password':forms.PasswordInput}
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
         fields=['address','profile_pic']
+        widgets={'address':forms.Textarea(attrs={'cols':20,'rows':5})}
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
 
 from app.forms import *
 
@@ -8,4 +9,19 @@ def registration(request):
     ufo=UserForm()
     pfo=ProfileForm()
     d={'ufo':ufo,'pfo':pfo}
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            MUFDO=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+            MPFDO=pfd.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            return HttpResponse('Registration done successfully...')
+        else:
+            return HttpResponse('Invalid data....')
+
     return render(request,'registration.html',d)
